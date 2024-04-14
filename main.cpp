f49b04c99fcdc79d25272368b27b37ade7560fdd
Sujan2468 <sujanthapal2468@gmail.com>
2024-04-14 20:06:08 +0545

    Insertion of main.cpp files.

---
+++ main.cpp
@@ -0,0 +1,18 @@
+/* program to find factoiral of a number. */
+#include<iostream>
+using namespace std;
+int factorial(int num)
+{
+if(num==1||num==0)
+return 1;
+return num*factorial(num-1);
+}
+int main()
+{
+int num;
+cout<<"Enter a number : ";
+cin>>num;
+int fact=factorial(num);
+cout<<"Factorial of "<<num<<" is "<<fact<<"."<<endl;
+return 0;
+}
